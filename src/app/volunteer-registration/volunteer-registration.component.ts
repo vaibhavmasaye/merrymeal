ca502e6b4d83dadb9aa81ba9bcd44cc7e6822de4
akashagadi <akashagadi1@gmail.com>
2022-03-08 15:48:36 +0530

    first commit

---
+++ src/app/volunteer-registration/volunteer-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteer-registration',
+  templateUrl: './volunteer-registration.component.html',
+  styleUrls: ['./volunteer-registration.component.css']
+})
+export class VolunteerRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
