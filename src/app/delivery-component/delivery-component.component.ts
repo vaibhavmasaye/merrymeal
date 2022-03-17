60b6bf685b2c53ac16c9916306b8daa424ab1f72
mrunal <mrunal@opoyi.com>
2022-03-17 11:39:05 +0530

    edit and delte menu

---
+++ src/app/delivery-component/delivery-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-component',
+  templateUrl: './delivery-component.component.html',
+  styleUrls: ['./delivery-component.component.css']
+})
+export class DeliveryComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
