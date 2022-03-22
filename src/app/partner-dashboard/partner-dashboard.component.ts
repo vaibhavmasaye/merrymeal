2d9fa66919ac622242f5da2c4de317d9ebee783d
Gaurav Solanki <solankigaurav433@gmail.com>
2022-03-22 21:01:14 +0530

    partner dashboard working properly

--- src/app/partner-dashboard/partner-dashboard.component.ts
+++ src/app/partner-dashboard/partner-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-partner-dashboard',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartnerDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+  createMenu= function(){
+    this.router.navigateByUrl('/create-menu')
   }
 
 }
